1efce8e32f8cb334aa21ed93027bcca35c5b942d
Yoran Brondsema <yoran@hstry.co>
2016-12-14 09:27:15 +0100

    Added tests for #reduceAndSanitizeAttributes

--- tests/unit/utils/sanitization-utils-test.js
+++ tests/unit/utils/sanitization-utils-test.js
@@ -1,7 +1,8 @@
 /* global QUnit */
 
 import {
-  sanitizeAttributeValue
+  sanitizeAttributeValue,
+  reduceAndSanitizeAttributes
 } from 'mobiledoc-dom-renderer/utils/sanitization-utils';
 
 const { test, module } = QUnit;
@@ -32,5 +33,36 @@ test('#sanitizeAttributeValue - a', (assert) => {
   for (let i = 0; i < safe.length; i++) {
     let url = safe[i];
     assert.equal(sanitizeAttributeValue('href', url, 'a'), url);
+  }
+});
+
+test('#reduceAndSanitizeAttributes - a', (assert) => {
+  let unsafe = [
+    'javascript:alert("XSS")', // jshint ignore: line
+    'vbscript:alert("XSS")' // jshint ignore: line
+  ];
+
+  for (let i = 0; i < unsafe.length; i++) {
+    let url = unsafe[i];
+    assert.deepEqual(reduceAndSanitizeAttributes(['href', url], 'a'), {
+      'href': `unsafe:${url}`
+    });
+  }
+
+  let safe = [
+    'http://www.google.com',
+    'https://www.google.com',
+    'ftp://google.com',
+    'http://www.google.com/with-path',
+    'www.google.com',
+    'tel:12345',
+    'mailto:john@doe.com'
+  ];
+
+  for (let i = 0; i < safe.length; i++) {
+    let url = safe[i];
+    assert.deepEqual(reduceAndSanitizeAttributes(['href', url], 'a'), {
+      'href': url
+    });
   }
 });
